ae6328488004faa306d499b0e075265af7c6edaf
Sabhnam Saha <128620969+SabhnamSaha-MCA-A-2024-26-39-UEMK@users.noreply.github.com>
2025-01-21 12:20:00 +0530

    print the alternate digits

---
+++ python/alternate_digits.py
@@ -0,0 +1,7 @@
+#WAPto input a 10-digit number and print the alternate digits
+num = input("Enter a 10-digit number: ")
+if len(num) == 10 and num.isdigit():
+    alternate_digits = num[::2]
+    print("Alternate digits:", alternate_digits)
+else:
+    print("Please enter a valid 10-digit number.")
