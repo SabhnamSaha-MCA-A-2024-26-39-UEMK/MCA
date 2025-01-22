07dbc19b5a1fa628243d9d8a487fe873cb79934f
Sabhnam Saha <128620969+SabhnamSaha-MCA-A-2024-26-39-UEMK@users.noreply.github.com>
2025-01-22 13:58:53 +0530

    create 2D array and access the array element

---
+++ Java/array_2d_access.java
@@ -0,0 +1,12 @@
+public class array_2d_access {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        System.out.println("Element at [0][0]: " + matrix[0][0]);
+        System.out.println("Element at [1][2]: " + matrix[1][2]);
+        System.out.println("Element at [2][1]: " + matrix[2][1]);
+    }
+}
