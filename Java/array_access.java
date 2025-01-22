7e6b07ea57d29f8bcd38021b17df155dbd5eacd2
Sabhnam Saha <128620969+SabhnamSaha-MCA-A-2024-26-39-UEMK@users.noreply.github.com>
2025-01-22 13:57:35 +0530

    create a simple array and access array element.

---
+++ Java/array_access.java
@@ -0,0 +1,8 @@
+public class array_access {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        System.out.println("First element: " + numbers[0]);
+        System.out.println("Second element: " + numbers[1]);
+        System.out.println("Third element: " + numbers[2]);
+    }
+}
