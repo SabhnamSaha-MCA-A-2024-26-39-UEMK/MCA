a4924da072de21f925a0a3cc9fd4514aab3c5527
Sabhnam Saha <128620969+SabhnamSaha-MCA-A-2024-26-39-UEMK@users.noreply.github.com>
2025-01-30 11:56:15 +0530

    list tuple dictionary

---
+++ python/list_tuple_dict.py
@@ -0,0 +1,12 @@
+thislist = ("apple", "banana", "cherry","sabhnam", 8)
+print(thislist)
+
+thistuple = ("apple", "banana", "cherry","sabhnam", 8)
+print(thistuple)
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Ford",
+  "year": 1964
+}
+print(thisdict)
